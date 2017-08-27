25653f0e61d85a3e62c1e9a35c4f70e9ffa6987a
Mike-Laptop <michaelengelandrease@gmail.com>
2017-08-27 13:04:44 -0400

    added some variables for flexibility

--- image-finder.sh
+++ image-finder.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
+# Author: Mike https://guides.wp-bullet.com
+# Purpose: Search for unused duplicate images
 
-# enter path to search for dupes
-DUPEPATH=""
+# enter path to search for dupes, cannot be absolute path!
+DUPEPATH="wp-content/uploads/"
+
 # enter path to store dupes that can be removed
 DUPELOG="/tmp/dupelog"
+
+# empty DUPELOG
+cat /dev/null > "$DUPELOG"
 
 # create dupes array after trimming empty lines
 DUPESARRAY=($(fdupes --recurse "$DUPEPATH" | sed '/^$/d'))
 
 # loop through array and search database
-for DUPE in ${DUPESARRAY[@]};
+for DUPE in "${DUPESARRAY[@]}";
 do
     # no skip columns flag, ask wp-cli channel
     echo "Searching for $DUPE"
@@ -18,10 +24,10 @@ do
     if [ "$DUPEFIND" = 0 ]; then
 
         #DUPEFINDESCAPED use sed to replace / with \/ and search for that too!
-        DUPEFINDESCAPED=$(wp db search $(echo "$DUPE" | sed 's#/#\\/#g') --stats --allow-root | awk '{print $3}')
-
+#        DUPEFINDESCAPED=$(wp db search "$(echo "$DUPE" | sed 's#/#\\/#g')" --stats --allow-root | awk '{print $3}')
+        DUPEFINDESCAPED=$(wp db search "${DUPE////\\/}" --stats --allow-root | tail -1 | awk '{print $3}')
         if [ "$DUPEFINDESCAPED" = 0 ]; then
-            echo $DUPE >> /tmp/herp
+            echo "$DUPE" >> "$DUPELOG"
             echo "Not in database"
         fi
     else
@@ -29,5 +35,6 @@ do
     fi
 done
 
-# show how much can be saved
-echo $(du -sh < "$DUPELOG") can be saved
+# show how much space can be saved by removing dupes
+echo "$(du -sh < "$DUPELOG") can be saved"
+
